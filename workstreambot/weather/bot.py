b5891062509cca91d52f037349350b4338afee49
Etjen Ymeraj <etjenymeraj@outlook.com>
2017-11-11 18:26:09 +0100

    Comments added

--- workstreambot/weather/bot.py
+++ workstreambot/weather/bot.py
@@ -5,9 +5,10 @@ from rasa_core.actions import Action
 
 
 class ActionSearchWeather(Action):
+    # the name should match the action to the utterance
     def name(self):
         return 'action_search_weather'
-
+    # the run executes when the action is called 
     def run(self, dispatcher, tracker, domain):
         dispatcher.utter_message("here's what the wheather looks like")
         return []
