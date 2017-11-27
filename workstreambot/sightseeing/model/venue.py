3477d9a71675b615b9960a9ba564caef3acfeddb
Etjen Ymeraj <etjenymeraj@outlook.com>
2017-11-27 19:07:11 +0100

    Initial venue model

---
+++ workstreambot/sightseeing/model/venue.py
@@ -0,0 +1,8 @@
+class Venue(object):
+    # init method
+    def __init__(self, venue_data):
+        self._venue_data = weather_data
+
+    # Parse the name of the city
+    def getVenueName(self):
+        return self._venue_data['name']
