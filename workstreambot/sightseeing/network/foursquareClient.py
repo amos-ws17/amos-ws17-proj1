07fc3900e62616c45495c3eb77eb3a90c4aa6ebb
Etjen Ymeraj <etjenymeraj@outlook.com>
2017-11-28 11:03:08 +0100

    Tips model and functionality added

--- workstreambot/sightseeing/network/foursquareClient.py
+++ workstreambot/sightseeing/network/foursquareClient.py
@@ -1,6 +1,7 @@
 import network.foursquareConstants as C
 from network.apiClient import APIClient
 from model.venue import Venue
+from model.venueTips import VenueTips
 
 class FoursquareClient(object):
     # define the base url for the API
@@ -30,6 +31,30 @@ class FoursquareClient(object):
             [venues.append(Venue(place)) for place in jsonResponse['response']['venues']]
             # return the array of all venue models
             return venues
+        else:
+            # return the jsonResponse
+            return jsonResponse
+
+    def fetch_tips_for_venue(self, venue_id):
+        # define parameters
+        parameters = {C.foursquareGeneralKeys['Client']: C.foursquareGeneralKeysValues['Client_ID'],
+                      C.foursquareGeneralKeys['Secret']: C.foursquareGeneralKeysValues['Secret_ID'],
+                      C.foursquareGeneralKeys['Version']: C.foursquareGeneralKeysValues['Version_ID'],
+                      C.foursquareParamterKeys['Limit']: C.foursquareParamterKeysValues['Limit_ID']}
+        # add the method to the url
+        tipsURL = self.baseurl + C.foursquareMethodKeys['Venue_ID'] + venue_id + C.foursquareMethodKeys['Tips']
+        # init APIClient
+        api = APIClient()
+        # make the url call and retrieve a json Response
+        jsonResponse = api.fetch(tipsURL, parameters)
+        # check if the jsonResponse contains an object and pass it to the weather model
+        if int(jsonResponse['response']['tips']['count']) > 0:
+            # empty array to store the tips dictionary
+            tips = []
+            # init a venue tip model with an object and pass that to the array
+            [tips.append(VenueTips(tip)) for tip in jsonResponse['response']['tips']['items']]
+            # return the array of all venue models
+            return tips
         else:
             # return the jsonResponse
             return jsonResponse
