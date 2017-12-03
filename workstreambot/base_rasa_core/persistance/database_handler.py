30db109ad7e0ccabfc910d51224f29ee90dbf07a
Radoslav Vlaskovski <radoslav.vlaskovsky@gmail.com>
2017-12-03 17:30:55 +0100

    first version of mongo handler

---
+++ workstreambot/base_rasa_core/persistance/database_handler.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+
+
+class MongoDBHandler:
+
+
+    def __init__(self, mongo_ip="192.168.99.100", mongo_port=27017):
+        self.mongo_ip = mongo_ip
+        self.mongo_port = mongo_port
+        self.client =  MongoClient("192.168.99.100", 27017)
+
+    def get_client(self):
+        return self.client
+
+    def write_data(self, data, collection_name, db_name):
+
+        db = self.client[db_name]
+        collection = db.get_collection(collection_name)
+        collection.insert_one(data)
+
+    def find_in_collection(self, key, collection_name, db_name):
+        db = self.client[db_name]
+        collection = db.get_collection(collection_name)
+        objs = collection.find(key).sort("name")
+        return objs
+
+    def find_one_in_collection(self, key, collection_name, db_name):
+        db = self.client[db_name]
+        collection = db.get_collection(collection_name)
+        obj = collection.find_one(key)
+        return obj
+
+    def find_all_in_collection(self, collection_name, db_name):
+        db = self.client[db_name]
+        collection = db.get_collection(collection_name)
+        all = collection.find()
+        return all
+
+    def get_collection_size(self, collection_name, db_name):
+        db = self.client[db_name]
+        collection = db.get_collection(collection_name)
+        return collection.find({}).count()
