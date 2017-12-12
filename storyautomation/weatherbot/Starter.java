4d9f9793b61ac1bb4b649a2a95ef4e46f8f23e50
Veselin Veselinov Popov <veselin.v.popov@campus.tu-berlin.de>
2017-12-12 16:50:50 +0100

    Modified the algorithm for generating of dialogue stories. It works also for graphs with loops (with some exceptions)

--- storyautomation/weatherbot/Starter.java
+++ storyautomation/weatherbot/Starter.java
@@ -1,44 +1,44 @@
-package amos.sprint8;
+package amos.storyautomation.weather;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Starter2 {
+public class Starter {
 	public static void main(String[] args) {
 		
 		List<String> weatherActions = new ArrayList<>();
 		weatherActions.add("_greet[]");
 		weatherActions.add("utter_greet");
 		weatherActions.add("utter_ask_howcanhelp");
-		weatherActions.add("_inform[location,date]");
+		weatherActions.add("_inform[location,time]");
 		weatherActions.add("utter_on_it");
 		weatherActions.add("utter_ask_location");
 		weatherActions.add("_inform[location]");
 		weatherActions.add("utter_ack_dosearch");
 		weatherActions.add("action_search_weather");
 		weatherActions.add("utter_ask_helpmore");
-		weatherActions.add("_thankyou[]");
+		weatherActions.add("_goodbye[]");
 		weatherActions.add("utter_goodbye");
 		
-		Graph4 flowChart = new Graph4(weatherActions);
+		GraphWeather flowChart = new GraphWeather(weatherActions);
 
 		flowChart.addEdge("_greet[]", "utter_greet");
 		flowChart.addEdge("utter_greet", "utter_ask_howcanhelp");
-		flowChart.addEdge("utter_ask_howcanhelp", "_inform[location,date]");
-		flowChart.addEdge("_inform[location,date]", "utter_on_it");
+		flowChart.addEdge("utter_ask_howcanhelp", "_inform[location,time]");
+		flowChart.addEdge("_inform[location,time]", "utter_on_it");
 		flowChart.addEdge("utter_on_it", "utter_ask_location");
 		flowChart.addEdge("utter_on_it", "utter_ack_dosearch");
 		flowChart.addEdge("utter_ask_location", "_inform[location]");
 		flowChart.addEdge("_inform[location]", "utter_ack_dosearch");
 		flowChart.addEdge("utter_ack_dosearch", "action_search_weather");
 		flowChart.addEdge("action_search_weather", "utter_ask_helpmore");
-		flowChart.addEdge("utter_ask_helpmore", "_thankyou[]");
-		flowChart.addEdge("utter_ask_helpmore", "_inform[location,date]");
-		flowChart.addEdge("_thankyou[]", "utter_goodbye");
+		flowChart.addEdge("utter_ask_helpmore", "_goodbye[]");
+		flowChart.addEdge("utter_ask_helpmore", "_inform[location,time]");
+		flowChart.addEdge("_goodbye[]", "utter_goodbye");
 		
-		String start = weatherActions.get(0), end = weatherActions.get(11);
+		String start = weatherActions.get(0), end = weatherActions.get(weatherActions.size() - 1);
 //		System.out.println("Following are all different paths from " + start + " to " + end);
 		flowChart.printAllPaths(start, end);
 	}
