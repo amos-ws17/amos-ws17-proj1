3c0e3f8e50f2e266903c80bf2304746a96e63d0a
lukas <l.kleinebuening@campus.tu-berlin.de>
2017-12-16 18:32:41 +0100

    removed unnecessary action and entities

--- workstreambot/scrum/action.py
+++ workstreambot/scrum/action.py
@@ -1,10 +1,10 @@
 from rasa_core.actions import Action
 
 # the themes that scrum can talk about, should be persisted in the DB
-theme_list = ["roles", "stories", "sprint", "ceremonies", "backlog", "estimations",
+theme_list = ["scrum", "roles", "stories", "sprint", "ceremonies", "backlog", "estimations",
               "release", "burndown", "velocity", "extras", "spike", "goals"]
 # the explanations of the themes
-theme_dict = {"roles": "PO, SM, DEV", "stories": "", "sprint": "", "ceremonies": "", "backlog": "",
+theme_dict = {"scrum": "", "roles": "PO, SM, DEV", "stories": "", "sprint": "", "ceremonies": "", "backlog": "",
               "estimations": "", "release": "", "burndown": "", "velocity": "", "extras": "",
               "spike": "", "goals": ""}
 
@@ -18,7 +18,7 @@ def getNextElement(theme):
         current_index += 1
         return theme_list[current_index]
     except IndexError:
-        return ""
+        return None
 
 # ask to continue to the next theme
 class ActionAskContinue(Action):
@@ -44,12 +44,4 @@ class ActionExplain(Action):
     def run(self, dispatcher, tracker, domain):
         # explain the current theme
         dispatcher.utter_message(theme_dict[current_theme])
-        return []
-
-class ActionEndGuide(Action):
-    def name(self):
-        return 'utter_end_guide'
-
-    def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message("That is it for the crash course in scrum")
         return []
