9bdff500c69a902aea3791ac5aafdcb992e1099e
lukas <l.kleinebuening@campus.tu-berlin.de>
2018-01-13 00:03:29 +0100

    Session handling bug fix

--- workstreambot/session.py
+++ workstreambot/session.py
@@ -1,15 +1,5 @@
 class Session:
-    dialogue_models = None
     current_dialogue_topic = None
-
-    def __init__(self, agents):
-        self.dialogue_models = agents
-
-    def set_dialogue_models(self, agents):
-        self.dialogue_models = agents
-
-    def get_dialogue_models(self):
-        return self.dialogue_models
 
     def set_current_dialogue_topic(self, current_dialogue):
         self.current_dialogue_topic = current_dialogue
